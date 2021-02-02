1dc905d8e04870fe10e653b82f433307fa4cd66a
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-02-02 10:41:38 -0800

    establish the form function of the UI.

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function(){
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const number3 = parseInt($("#input3").val());
+    
+    let result;
+    if (operator === "add"){
+      result = add(number1, number2);
+    } else if (operator === "subtract"){
+      result = subtract(number1, number2);
+    } else if (operator === "multiply"){
+      result = multiplication(number1, number2);
+    } else if (operator === "divide"){
+      result = divide(number1, number2)
+    }
+    
+    $("#output").text(result);
+  });
+});
+
+/*const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2){
+  return number1 - number2;
+};
+
+const multiplication = function(number1, number2){
+  return number1*number2;
+};
+
+const divide = function(number1, number2){
+  return number1/number2;
+};
+*/
