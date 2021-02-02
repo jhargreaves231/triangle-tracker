6f371f14fec6ad22faf86824d7318df35f605480
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-02-02 13:53:45 -0800

    update all functionality to work with determining: not a triangle, equilateral, isosceles, and scalene.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 /*function equilateral(var1, var2, var3) {
-  if (var1 === var2 === var3) {
+  if (var1 === var2 & var1 === var3 & var2 === var3) {
     return "Equilateral"
   }
   else {
     return "Not Equilateral"
+  }  
+}*/
+
+/*function isosceles(var1, var2, var3) {
+  if (var1 === var2 || var2 === var3 || var1 === var3){
+    return "Isosceles"
+  }
+  else {
+    return "Not Isoceles"
+  }
+}*/
+
+
+/*function equilateral(var1, var2, var3) {
+  if (var1 === var2 & var1 === var3 & var2 === var3) {
+    return "Equilateral"
+  } else if (var1 === var2 || var2 === var3 || var1 === var3) {
+    return "Isosceles"
+  }
+  else {
+    return "Scalene"
   }
 }*/
 
 function equilateral(var1, var2, var3) {
-  if (var1 === var2 === var3) {
+  if (var1 + var2 <= var3 || var2 + var3 <= var1 || var1 + var3 <= var2){
+    return "Not A Triangle"
+  } else if (var1 === var2 & var1 === var3 & var2 === var3) {
     return "Equilateral"
   } else if (var1 === var2 || var2 === var3 || var1 === var3) {
     return "Isosceles"
@@ -18,19 +41,26 @@ function equilateral(var1, var2, var3) {
   }
 }
 
+/*function notTriangle (var1, var2, var3) {
+  if (var1 + var2 <= var3 || var2 + var3 <= var1 || var1 + var3 <= var2){
+    return "Not A Triangle"
+  }
+}*/
+
 
 // Everything below this line is user interface logic:
 
 $(document).ready(function(){
-  $("form#calculator").submit(function(event) {
+  $("form#triangle").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const number3 = parseInt($("#input3").val());
     
     let result = equilateral(number1, number2, number3)
+    //let result2 = isosceles(number1, number2, number3)
 
-       $("#output").text(result);
+    $("#output").text(result);
   });
 });
 
